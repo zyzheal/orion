8bb1102c6b6560ceb275b2da6b4a59464deb8f8a
heal <healscm@gmail.com>
2026-09-12 18:53:59 +0800

    chore(tenantutil): 删除 1 个死代码包 (整个模块)

--- orion-platform-svc-go/internal/tenantutil/interfaces.go
+++
@@ -1,9 +0,0 @@
-package tenantutil
-
-type TenantutilRepository interface {
-	FindByID(ctx interface{}, id string) (interface{}, error)
-}
-
-type TenantutilService interface {
-	GetByTenantID(ctx interface{}, tenantID string) (interface{}, error)
-}
--- orion-platform-svc-go/internal/tenantutil/models_json.go
+++
@@ -1,7 +0,0 @@
-package tenantutil
-
-type TenantConfig struct {
-	TenantID string `json:"tenant_id"`
-	Name     string `json:"name"`
-	Enabled  bool   `json:"enabled"`
-}
