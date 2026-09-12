403d90ad0ddb79161271a58dce4ccac6197b116e
heal <healscm@gmail.com>
2026-09-12 11:24:58 +0800

    chore(cache-mgmt): 删除 2 个死代码包

--- orion-platform-svc-go/internal/cache-mgmt/config/config.go
+++
@@ -1,9 +0,0 @@
-package config
-
-// Config holds module-level configuration for cache-mgmt.
-type Config struct{}
-
-// Load returns a default configuration.
-func Load() *Config {
-	return &Config{}
-}
