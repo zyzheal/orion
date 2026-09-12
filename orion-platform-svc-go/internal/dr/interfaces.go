2b28ebfec32dcfcca004da7c82746152599bd101
heal <healscm@gmail.com>
2026-09-12 12:18:19 +0800

    chore(dr): 删除 4 个死代码包 (整个模块)

--- orion-platform-svc-go/internal/dr/interfaces.go
+++
@@ -1,9 +0,0 @@
-package dr
-
-type DRRepository interface {
-	FindByID(ctx interface{}, id string) (interface{}, error)
-}
-
-type DRService interface {
-	GetByID(ctx interface{}, id string) (interface{}, error)
-}
