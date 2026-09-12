d26d7d8ce98d5c970f02eca984ed390dc9672b99
heal <healscm@gmail.com>
2026-09-12 12:49:27 +0800

    chore(logging): 删除 1 个死代码包 (otel)

--- orion-platform-svc-go/internal/logging/otel/log.go
+++
@@ -1,9 +0,0 @@
-package otel
-
-import "log"
-
-// otelLogf is a shared debug logger used by InitTracerProvider and
-// InitMeterProvider when they fall back to no-op mode.
-func otelLogf(format string, args ...interface{}) {
-	log.Printf(format, args...)
-}
