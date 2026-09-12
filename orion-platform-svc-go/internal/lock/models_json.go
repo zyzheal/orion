8f982a1022d6a27ffc4c417436cb208b5600aaeb
heal <healscm@gmail.com>
2026-09-12 12:47:56 +0800

    chore(lock): 删除 1 个死代码包

--- orion-platform-svc-go/internal/lock/models_json.go
+++
@@ -1,8 +0,0 @@
-package lock
-
-type LockMetadata struct {
-	Key      string `json:"key"`
-	TenantID string `json:"tenant_id"`
-	Holder   string `json:"holder"`
-	Expired  bool   `json:"expired"`
-}
